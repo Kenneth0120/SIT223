7772f425da68a1890df200f3f81188dcdf7122ea
Kenneth0120 <blade_doodle@hotmail.com>
2024-08-23 13:49:30 +1000

    Testing Jenkins pipeline

---
+++ code.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// update to push Jenkins 1 - 10:36am
+// test 2 - updated webhook - 10:44am
